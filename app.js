e6597095f69d30eae75c26ea31ee9cd4a0b0df9f
AFRIKAN BACKEND DEVELOPER <christopherleon237@gmail.com>
2023-06-17 19:29:32 +0100

    updated for deployment

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const mongoose=require("mongoose")
+const dotenv=require("dotenv")
+dotenv.config({path: "./dotenv.config"})
 const app=require("./middlewares")
 const authRoutes=require("./Routes/authRoutes")
 const productsRoutes=require("./Routes/productsRoutes")
@@ -20,4 +23,16 @@ app.use("/v1/api/stats", statsRoutes)
 
 
 
-module.exports=app;
+
+const DB=process.env.DATABASE
+const port=process.env.PORT
+mongoose.connect(DB, {
+    useNewUrlParser: true
+}).then(con =>{
+    console.log(`Database connected successfully on port ${port}`)
+}).catch(err =>{
+    console.log(`There is error in connecting the database to port ${port}`, err)
+})
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}...🏃`)
+})
